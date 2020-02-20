2444b656fd9429018f298053017407191633b3f4
Brendon Dulam <brendondulam06@gmail.com>
2020-02-20 21:57:40 +0900

    final solution to maxPoints runtime: 0ms memory: 2.1 MB

--- maxPointsOnALine/problem149.go
+++ maxPointsOnALine/problem149.go
@@ -1,89 +1,69 @@
 package maxPointsOnALine
 
-import (
-	"fmt"
-	"math"
-)
-
 type Point struct {
 	x int
 	y int
 }
 
 type Line struct {
-	p1         Point
-	p2         Point
-	slope      float64
-	yIntercept float64
+	A int
+	B int
+	C int
 }
 
-func (r *Line) getSlope() float64 {
-	if r.slope == 0 && r.p1.y != r.p2.y {
-		deltaY := float64(r.p2.y - r.p1.y)
-		deltaX := float64(r.p2.x - r.p1.x)
+func (r Line) isCollinear(p Point) bool {
+	return r.A*p.x+r.B*p.y+r.C == 0
+}
 
-		if deltaY != 0 {
-			if deltaX == 0 {
-				return math.MaxFloat64
-			}
-			r.slope = deltaY / deltaX
-		}
+func getLine(p1 Point, p2 Point) Line {
+	A, B := p1.y-p2.y, -(p1.x - p2.x)
+	if A < 0 {
+		A, B = -A, -B
 	}
 
-	return r.slope
+	return Line{A, B, -(A*p1.x + B*p1.y)}
 }
 
-func (r *Line) getYIntercept() float64 {
-	if r.yIntercept == 0 {
-		r.yIntercept = float64(r.p1.y) - r.getSlope()*float64(r.p1.x)
-	}
-
-	return r.yIntercept
-}
-
-func MaxPointsOnALine(points [][]int) int {
+func MaxPoints(points [][]int) int {
 	pointsLen := len(points)
-
 	if pointsLen < 3 {
 		return pointsLen
 	}
-	maxLen := 2
 
-	pointsMap := make(map[int]map[int]int)
-	linesMap := make(map[float64]map[float64][][]int)
-
+	maxLineLen := 0
+	maxPointsLen := 0
+	lines := make(map[Line]bool)
+	collinearPoints := make(map[Point]int)
 	for i := 0; i < pointsLen; i++ {
-		p1 := Point{points[i][0], points[i][1]}
-		if pointsMap[p1.x] == nil {
-			pointsMap[p1.x] = make(map[int]int)
-		}
-		pointsMap[p1.x][p1.y]++
-
 		for j := i + 1; j < pointsLen; j++ {
+			p1 := Point{points[i][0], points[i][1]}
 			p2 := Point{points[j][0], points[j][1]}
+			if p1 != p2 {
+				line := getLine(p1, p2)
+				if !lines[line] {
+					numPoints := 0
+					for _, point := range points {
+						if line.isCollinear(Point{point[0], point[1]}) {
+							numPoints++
+						}
+					}
 
-			if !(p1.x == p2.x && p1.y == p2.y) {
-				l := Line{p1: p1, p2: p2}
-				slope, yInt := l.getSlope(), l.getYIntercept()
-
-				if slope == -1 {
-					fmt.Println(l)
+					if numPoints > maxLineLen {
+						maxLineLen = numPoints
+					}
 				}
-
-				if linesMap[slope] == nil {
-					linesMap[slope] = make(map[float64][][]int)
+			} else {
+				collinearPoints[p1]++
+				if maxPointsLen < collinearPoints[p1] {
+					maxPointsLen = collinearPoints[p1]
 				}
-				linesMap[slope][yInt] = append(linesMap[slope][yInt], points[j])
-
-				if maxLen < len(linesMap[slope][yInt]) {
-					maxLen = len(linesMap[slope][yInt])
-				}
-				//fmt.Println(i, j, l, l.getSlope(), l.getYIntercept())
 			}
 		}
 	}
 
-	fmt.Println(pointsMap)
-	fmt.Println(linesMap)
-	return maxLen
+	if maxPointsLen > maxLineLen {
+		return maxPointsLen
+	}
+
+	return maxLineLen
 }
